fffd49fc29c1510a2bbf87cdc0659acbb19a31a4
DESKTOP-P1J35I2\sysuser <cipri91rusu@gmail.com>
2021-07-13 01:06:32 +0300

    Updated default exports in component

--- index.js
+++ index.js
@@ -9,5 +9,19 @@ var __createBinding = (this && this.__createBinding) || (Object.create ? (functi
 var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.ColumnTypes = exports.ColumnCollapsable = exports.ColumnSizes = exports.MinimumVisibility = exports.Grid = void 0;
+const Grid_1 = __importDefault(require("./Grid/Grid"));
+exports.Grid = Grid_1.default;
+const ColumnVisibility_1 = require("./Grid/CustomTypes/ColumnVisibility");
+Object.defineProperty(exports, "MinimumVisibility", { enumerable: true, get: function () { return ColumnVisibility_1.MinimumVisibility; } });
+const ColumnSizes_1 = require("./Grid/CustomTypes/ColumnSizes");
+Object.defineProperty(exports, "ColumnSizes", { enumerable: true, get: function () { return ColumnSizes_1.ColumnSizes; } });
+const ColumnCollapsable_1 = require("./Grid/CustomTypes/ColumnCollapsable");
+Object.defineProperty(exports, "ColumnCollapsable", { enumerable: true, get: function () { return ColumnCollapsable_1.ColumnCollapsable; } });
+const ColumnTypes_1 = require("./Grid/CustomTypes/ColumnTypes");
+Object.defineProperty(exports, "ColumnTypes", { enumerable: true, get: function () { return ColumnTypes_1.ColumnTypes; } });
 __exportStar(require("./Grid/Grid"), exports);
--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,24 @@
+import Grid from "./Grid/Grid";
+import { IHeader } from "./Grid/Interfaces/GridBody/IHeader";
+import { IFilter } from "./Grid/Interfaces/GridTools/IFilter";
+import { IGrouping } from "./Grid/Interfaces/GridBody/IGrouping";
+import { IColumnOptions } from "./Grid/Interfaces/GridBody/IColumnOptions";
+import { MinimumVisibility } from "./Grid/CustomTypes/ColumnVisibility";
+import { ColumnSizes } from "./Grid/CustomTypes/ColumnSizes";
+import { ColumnCollapsable } from "./Grid/CustomTypes/ColumnCollapsable";
+import { ColumnTypes } from "./Grid/CustomTypes/ColumnTypes";
+import { IColumn } from "./Grid/Interfaces/GridBody/IColumn";
+
+export { 
+    Grid, 
+    IHeader, 
+    IFilter, 
+    IColumn, 
+    IGrouping, 
+    IColumnOptions, 
+    MinimumVisibility, 
+    ColumnSizes, 
+    ColumnCollapsable, 
+    ColumnTypes }
+
 export * from "./Grid/Grid";
